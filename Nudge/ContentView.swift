2d8600e965ad1b5b61fef9459203fdbba59d7b0a
Linnéa <linnea.ternevik@gmail.com>
2026-04-28 11:34:29 +0200

    init: project setup

---
+++ Nudge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nudge
+//
+//  Created by Linnéa on 2026-04-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nudge/NudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NudgeApp.swift
+//  Nudge
+//
+//  Created by Linnéa on 2026-04-28.
+//
+
+import SwiftUI
+
+@main
+struct NudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
