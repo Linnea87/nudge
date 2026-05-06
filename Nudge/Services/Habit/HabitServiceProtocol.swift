c86bfca117a024e1a0fa51fc5606e9a99ec88e77
Linnéa <linnea.ternevik@gmail.com>
2026-05-06 18:22:29 +0200

    feat: added updateHabit to HabitService, HabitServiceProtocol and HabitViewModel

--- Nudge/Services/Habit/HabitServiceProtocol.swift
+++ Nudge/Services/Habit/HabitServiceProtocol.swift
@@ -10,6 +10,7 @@ import Foundation
 protocol HabitServiceProtocol {
     func fetchHabits(for userId: String) async throws -> [Habit]
     func addHabit(_ habit: Habit) async throws
+    func updateHabit(_ habit: Habit) async throws
     func deleteHabit(_ habit: Habit) async throws
     func checkIn(_ habit: Habit, on date: Date) async throws
 }
